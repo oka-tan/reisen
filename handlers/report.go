31c835a517e42f0eb03ae881d847d26f2de937fe
oka-tan <oka-tan@tutanota.com>
2022-09-15 22:57:40 -0300

    Improving report insertion

--- handlers/report.go
+++ handlers/report.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"database/sql"
 	"net/http"
 	"reisen/config"
 	"reisen/db"
@@ -30,14 +31,36 @@ func ReportGET(db *bun.DB, conf config.Config) func(echo.Context) error {
 
 func ReportPOST(pg *bun.DB, conf config.Config) func(echo.Context) error {
 	return func(c echo.Context) error {
-		count, err := pg.NewSelect().
+		tx, err := pg.BeginTx(context.Background(), &sql.TxOptions{})
+
+		if err != nil {
+			model := map[string]interface{}{
+				"conf":    conf.TemplateConfig,
+				"message": "Internal server error",
+			}
+
+			return c.Render(http.StatusInternalServerError, "report-result", model)
+		}
+
+		count, err := tx.NewSelect().
 			Model(&db.Report{}).
 			Where("user_ip = ?", c.RealIP()).
 			Where("created_at > ?", time.Now().Add(-time.Hour)).
 			Where("NOT handled").
 			Count(context.Background())
 
+		if err != nil {
+			tx.Rollback()
+			model := map[string]interface{}{
+				"conf":    conf.TemplateConfig,
+				"message": "Internal server error",
+			}
+
+			return c.Render(http.StatusInternalServerError, "report-result", model)
+		}
+
 		if count >= 5 {
+			tx.Rollback()
 			model := map[string]interface{}{
 				"conf":    conf.TemplateConfig,
 				"message": "You can't make more than five reports per hour",
@@ -50,6 +73,34 @@ func ReportPOST(pg *bun.DB, conf config.Config) func(echo.Context) error {
 
 		postNumberString := c.Param("post_number")
 		postNumber, _ := strconv.ParseInt(postNumberString, 10, 64)
+
+		var post db.Post
+		err = tx.NewSelect().
+			For("NO UPDATE").
+			Model(&post).
+			Where("board = ?", board).
+			Where("post_number = ?", postNumber).
+			Scan(context.Background())
+
+		if err != nil {
+			tx.Rollback()
+			model := map[string]interface{}{
+				"conf":    conf.TemplateConfig,
+				"message": "Internal server error",
+			}
+
+			return c.Render(http.StatusInternalServerError, "report-result", model)
+		}
+
+		if post.Hidden {
+			tx.Rollback()
+			model := map[string]interface{}{
+				"conf":    conf.TemplateConfig,
+				"message": "Post has already been hidden",
+			}
+
+			return c.Render(http.StatusInternalServerError, "report-result", model)
+		}
 
 		userIP := c.RealIP()
 		reportType := c.FormValue("reportType")
@@ -66,15 +117,25 @@ func ReportPOST(pg *bun.DB, conf config.Config) func(echo.Context) error {
 			Handled:    false,
 		}
 
-		_, err = pg.NewInsert().
+		_, err = tx.NewInsert().
 			Model(&report).
 			On("CONFLICT (board, post_number, user_ip) DO UPDATE SET comment = EXCLUDED.comment, report_type = EXCLUDED.report_type, created_at = EXCLUDED.created_at").
 			Exec(context.Background())
 
 		if err != nil {
+			tx.Rollback()
 			model := map[string]interface{}{
 				"conf":    conf.TemplateConfig,
-				"message": "Internal server error inserting report",
+				"message": "Internal server error",
+			}
+
+			return c.Render(http.StatusInternalServerError, "report-result", model)
+		}
+
+		if err := tx.Commit(); err != nil {
+			model := map[string]interface{}{
+				"conf":    conf.TemplateConfig,
+				"message": "Internal server error",
 			}
 
 			return c.Render(http.StatusInternalServerError, "report-result", model)
